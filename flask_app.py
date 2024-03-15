104a35b58fda643720b18177b15e1075c1e251ac
naomyduarteg <naomyduarteg@gmail.com>
2024-03-15 08:46:38 -0300

    generating some figures

--- flask_app.py
+++ flask_app.py
@@ -3,12 +3,14 @@ import pickle
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__, template_folder='templates', static_folder='static') #
+app = Flask(__name__, template_folder='templates', static_folder='static')
 model = pickle.load(open('ml_model/model.pkl', 'rb'))
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
 
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -20,11 +22,12 @@ def predict():
     Suítes = int(request.form['Suítes'])
     Mobiliado = str(request.form['Mobiliado'])
     features = np.array([[Tipo, Dormitórios, Banheiros, Garagens, Bairro, Suítes, Mobiliado]])
-    df = pd.DataFrame(features, columns = ['Tipo', 'Dormitórios', 'Banheiros', 'Garagens', 'Bairro', 'Suítes', 'Mobiliado'])
+    df = pd.DataFrame(features, columns=['Tipo', 'Dormitórios', 'Banheiros', 'Garagens', 'Bairro', 'Suítes', 'Mobiliado'])
     prediction = model.predict(df)
     output = np.expm1(prediction[0])
     # return render_template('index.html', prediction_text=f'Valor calculado do aluguel: R${round(float(output),0)}')
     return f"Valor calculado do aluguel: R${int(output)}"
+
 
 if __name__ == "__main__":
     app.run()
